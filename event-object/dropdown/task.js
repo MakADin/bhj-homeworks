6020910319b7b01ce8dc4223b275da20743dd3a7
MakArs <makarkin.a.d@gmail.com>
2025-06-30 23:15:07 +0300

    first commit dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,26 @@
+const value = document.querySelector('.dropdown__value');
+const items = Array.from(document.querySelectorAll('.dropdown__item'));
+const list = document.querySelector('.dropdown__list');
+
+value.addEventListener('mouseover', addActiveClass);
+value.addEventListener('mouseout', removeActiveClass);
+
+
+list.addEventListener('mouseover', addActiveClass);
+list.addEventListener('mouseout', removeActiveClass);
+
+function addActiveClass() {
+  list.classList.add('dropdown__list_active');
+}
+
+function removeActiveClass() {
+  list.classList.remove('dropdown__list_active');
+}
+
+items.forEach(item => {
+  item.addEventListener('click', (event) => {
+    event.preventDefault();
+    value.textContent = event.target.innerText;
+    removeActiveClass()
+  })
+})
