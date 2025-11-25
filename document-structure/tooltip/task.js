a81d905a43d158c6fe3c1da2c3466e3756cd3125
MakArs <makarkin.a.d@gmail.com>
2025-11-25 09:32:14 +0300

    third commit - add getBoundingClientRect

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,20 +1,89 @@
 const links = [...document.querySelectorAll('.has-tooltip')];
-let currentTooltip = null;
+let divTooltip = null;
+let activeLink = null;
 
-const createTooltipHTML = (text) => {
-  const divTooltip = document.createElement('div');
+const updateTooltipPosition = () => {
+  if (activeLink) {
+    const activeLinkRect = activeLink.getBoundingClientRect();
+
+    const topPositionTooltip = activeLinkRect.top + window.pageYOffset + 25;
+    const leftPositionTooltip = activeLinkRect.left + window.pageXOffset;
+
+    divTooltip.style.top = `${topPositionTooltip}px`;
+    divTooltip.style.left = `${leftPositionTooltip}px`;
+  }
+};
+
+const handleClickOnLink = (event) => {
+  event.preventDefault();
+
+  const currentLink = event.currentTarget.closest('.has-tooltip');
+  const titleLink = currentLink.getAttribute('title');
+
+  if (currentLink === activeLink) {
+    activeLink = null;
+    divTooltip.classList.toggle('tooltip_active');
+    return;
+  }
+
+  if (activeLink && activeLink !== currentLink) {
+    divTooltip.classList.remove('tooltip_active');
+  }
+  activeLink = currentLink;
+
+  divTooltip.textContent = titleLink;
+  divTooltip.classList.add('tooltip_active');
+
+  updateTooltipPosition();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  divTooltip = document.createElement('div');
   divTooltip.classList.add('tooltip');
-  divTooltip.textContent = text;
-  return divTooltip;
-};
+  divTooltip.textContent = '';
+  document.body.appendChild(divTooltip);
+});
 
-const removeAllTooltips = () => {
-  const activeTooltips = [...document.querySelectorAll('.tooltip')];
-  activeTooltips.forEach((tip) => {
-    tip.remove();
-  });
-  currentTooltip = null;
-};
+links.forEach((link) => {
+  link.addEventListener('click', handleClickOnLink);
+});
+
+window.addEventListener('scroll', updateTooltipPosition);
+
+window.addEventListener('resize', updateTooltipPosition);
+
+/*  (event) => {
+    event.preventDefault();
+
+    const titleLink = event.currentTarget.getAttribute('title');
+
+    getPositionClickedLink(link);
+
+    divTooltip.textContent = titleLink;
+
+    if (!divTooltip.classList.contains('tooltip_active')) {
+      divTooltip.classList.add('tooltip_active');
+    } else if (divTooltip.textContent === titleLink) {
+      divTooltip.classList.toggle('tooltip_active');
+    }
+  } */
+
+/* ============================================================= */
+
+// const createTooltipHTML = (text) => {
+//   const divTooltip = document.createElement('div');
+//   divTooltip.classList.add('tooltip');
+//   divTooltip.textContent = text;
+//   return divTooltip;
+// };
+
+// const removeAllTooltips = () => {
+//   const activeTooltips = [...document.querySelectorAll('.tooltip')];
+//   activeTooltips.forEach((tip) => {
+//     tip.remove();
+//   });
+//   currentTooltip = null;
+// };
 
 const displayTooltip = (link, tooltip) => {
   link.insertAdjacentElement('afterend', tooltip);
@@ -26,7 +95,7 @@ const switchClassActiveTooltip = () => {
   currentTooltip = null;
 };
 
-/* Обработчик события click по ссылкам */
+/* Обработчик события click по ссылкам
 
 links.forEach((link) => {
   link.addEventListener('click', (event) => {
@@ -43,10 +112,10 @@ links.forEach((link) => {
       removeAllTooltips();
     }
 
-    const newTooltip = createTooltipHTML(titleLink);
     displayTooltip(link, newTooltip);
 
     currentTooltip = newTooltip;
     // console.log(Boolean(document.querySelector('.tooltip')));
   });
 });
+*/
